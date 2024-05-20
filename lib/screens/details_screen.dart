0ad5284b1fb7a2b65fed293d740fb54a721c698c
marco.celis <marco.celis@gmail.com>
2024-05-20 18:15:54 -0500

    init detail screen

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -4,10 +4,44 @@ class DetailsScreen extends StatelessWidget {
   const DetailsScreen({super.key});
   @override
   Widget build(BuildContext context) {
+    // TODO: Cambiar luego por una instancia de Pelicula
+    final String movie =
+        ModalRoute.of(context)!.settings.arguments.toString() ?? 'no-movie';
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text('Details Screen'),
+        body: CustomScrollView(
+      slivers: [
+        _CustomAppBar(),
+      ],
+    ));
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  const _CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.indigo,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: EdgeInsets.all(0),
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          color: Colors.black12,
+          child: Text(
+            'movie.title',
+            style: TextStyle(fontSize: 16),
+          ),
+        ),
+        background: FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'),
+          image: NetworkImage('https://via.placeholder.com/500x300'),
+          fit: BoxFit.cover,
         ),
       ),
     );
